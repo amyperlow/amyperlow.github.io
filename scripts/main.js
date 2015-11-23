a0ff09379aae0b8be1504da549f39568e03467ef
Jordalgo <jordalgo@spotify.com>
2015-11-23 15:43:56 -0500

    updated email
    Change-Id: I7d246d9bbc47edebfe5449df1ca3cf3e1faa731e

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 $('a[href*=#]').click((e) => {
-  let location = window.location;
-  let target = (e.target.nodeName === 'SPAN') ? e.target.parentElement : e.target;
+  const location = window.location;
+  const target = (e.target.nodeName === 'SPAN') ? e.target.parentElement : e.target;
 
-  if (location.pathname.replace(/^\//,'') === target.pathname.replace(/^\//,'') && location.hostname === target.hostname) {
-    var $target = $(target.hash);
-    $target = $target.length && $target || $('[name=' + target.hash.slice(1) +']');
+  if (location.pathname.replace(/^\//, '') === target.pathname.replace(/^\//, '') && location.hostname === target.hostname) {
+    let $target = $(target.hash);
+    $target = $target.length && $target || $('[name=' + target.hash.slice(1) + ']');
     if ($target.length) {
-      var targetOffset = Math.ceil($target.offset().top);
+      const targetOffset = Math.ceil($target.offset().top);
       $('html,body').animate({scrollTop: targetOffset}, 1500);
       return false;
     }
